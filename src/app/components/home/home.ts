4283c9aa6478f6f94a990f4131e3eb9a801dbc60
Hijab Zahra <zhijab408@gmail.com>
2026-05-13 15:49:39 +0500

    new changes

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +9,30 @@ import { Component } from '@angular/core';
 })
 export class Home {
 
+  private http = inject(HttpClient);
 
   sendEmail() {
   window.location.href = "mailto:zhijab408@example.com?subject=Hiring Request&body=Hi, I want to hire you for a project.";
 }
 
   downloadCV() {
-  const link = document.createElement('a');
-  link.href = 'assets/resume/Zahra1-Resume.pdf';
-  link.download = 'Zahra1-Resume.pdf';
-  link.target = '_blank';
-  link.click();
+  
+ this.http.get('/assets/resume/my-resume.pdf', { responseType: 'blob' })
+      .subscribe({
+        next: (blob) => {
+          const url = window.URL.createObjectURL(blob);
+          const link = document.createElement('a');
+          link.href = url;
+          link.download = 'my-resume.pdf';
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          window.URL.revokeObjectURL(url);   // Clean up
+        },
+        error: (err) => {
+          console.error('Download failed', err);
+          // Show user error message
+        }
+      });
 }
 }
