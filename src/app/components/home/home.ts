a3ca212af3f5f3616a708045b17c2984462c096d
Hijab Zahra <zhijab408@gmail.com>
2026-05-14 21:48:35 +0500

    hire me button functionality

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,27 +9,34 @@ import { Component, inject } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+// constructor( private router: Router) { }
 
-  private http = inject(HttpClient);
 
-  downloadCV() {
-  
- this.http.get('/resume/my-resume.pdf', { responseType: 'blob' })
-      .subscribe({
-        next: (blob) => {
-          const url = window.URL.createObjectURL(blob);
-          const link = document.createElement('a');
-          link.href = url;
-          link.download = 'my-resume.pdf';
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-          window.URL.revokeObjectURL(url);   // Clean up
-        },
-        error: (err) => {
-          console.error('Download failed', err);
-          // Show user error message
-        }
-      });
+// goToContact() {
+//   this.router.navigate(['/contact']);
+// }
 }
-}
+
+//   private http = inject(HttpClient);
+
+//    downloadCV()
+//   {
+//  this.http.get('/resume/my-resume.pdf', { responseType: 'blob' })
+//       .subscribe({
+//         next: (blob) => {
+//           const url = window.URL.createObjectURL(blob);
+//           const link = document.createElement('a');
+//           link.href = url;
+//           link.download = 'my-resume.pdf';
+//           document.body.appendChild(link);
+//           link.click();
+//           document.body.removeChild(link);
+//           window.URL.revokeObjectURL(url);   // Clean up
+//         },
+//         error: (err) => {
+//           console.error('Download failed', err);
+//           // Show user error message
+//         }
+//       });
+// }
+ 
