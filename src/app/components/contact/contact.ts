69d75460fc0eccc5b83e97ab0ebd5ed7ac0f670d
Hijab Zahra <zhijab408@gmail.com>
2026-08-03 13:09:08 +0500

    Complete portfolio redesign

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,9 +1,56 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css',
+  styleUrl: './contact.css'
 })
-export class Contact {}
+export class Contact {
+
+  contactData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
+
+  sendEmail(form: any) {
+
+    emailjs.send(
+      'service_ot7tson',
+      'template_cueog0g',
+      {
+        from_name: this.contactData.name,
+        from_email: this.contactData.email,
+        subject: this.contactData.subject,
+        message: this.contactData.message
+      },
+      'mkAzXrZRDJF3QozFM'
+    ).then(() => {
+
+      alert('✅ Message sent successfully!');
+
+      this.contactData = {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      };
+
+      form.resetForm();
+
+    }).catch((error) => {
+
+      console.error(error);
+
+      alert('❌ Failed to send message.');
+
+    });
+
+  }
+
+}
--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css',
+  styleUrl: './navbar.css'
 })
 export class NavbarComponent {
+
+  isScrolled = false;
+
   menuOpen = false;
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  @HostListener('window:scroll')
+
+  onScroll(){
+
+    this.isScrolled = window.scrollY > 40;
+
   }
+
+  toggleMenu(){
+
+    this.menuOpen = !this.menuOpen;
+
+  }
+
+  closeMenu(){
+
+    this.menuOpen = false;
+
+  }
+
+  scrollToContact(): void {
+
+  const section = document.getElementById('contact');
+
+  if (section) {
+
+    section.scrollIntoView({
+
+      behavior: 'smooth',
+      block: 'start'
+
+    });
+
+  }
+
+}
+
 }
